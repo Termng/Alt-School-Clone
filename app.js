9a501d13111bf628f1f3ef386e5d840127134704
Torah <95339155+Termng@users.noreply.github.com>
2023-11-07 18:39:50 +0100

    Added js dependency for countdown numbers

---
+++ app.js
@@ -0,0 +1,22 @@
+// let valueDisplays = document.querySelectorAll(".num");
+
+// let speed =  1000;
+
+// valueDisplays.forEach((counter) =>{
+//     function upData(){
+//         const target = Number (counter.getAttribute('data-val'))
+//         const count = Number(counter.innerText)
+//         const inc = target/speed
+//         if (count < target){
+//             counter.innerText= Math.floor(inc + count)
+//             setTimeout(upData, 1)
+
+//         }
+//         else {
+//             counter.innerText = target
+//         }
+
+//     }
+//     upData()
+// })
+
